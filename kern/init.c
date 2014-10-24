f72f1bdc979914d84e601c0a668612e366a916fa
CSE506 Student Account <cse506@vl169.cs.stonybrook.edu>
2014-10-24 00:37:25 -0400

    commit lab5 initial

--- kern/init.c
+++ kern/init.c
@@ -89,7 +89,6 @@ i386_init(void)
 //	ENV_CREATE(user_faultallocbad, ENV_TYPE_USER);
         ENV_CREATE(user_forktree, ENV_TYPE_USER);
 	ENV_CREATE(user_spin, ENV_TYPE_USER);
-#endif // TEST*
 //	ENV_CREATE(user_yield, ENV_TYPE_USER);
 	// Schedule and run the first user environment!
 	sched_yield();
